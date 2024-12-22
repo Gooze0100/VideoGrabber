fed3ca4628fb9ee5c034671e2ba006f71451f90d
Giedrius <kalniusg@gmail.com>
2024-12-22 13:56:08 +0200

    Added functions for getting video.

--- VideoGrabber/VideoGrabberLibrary/Download.cs
+++ VideoGrabber/VideoGrabberLibrary/Download.cs
@@ -1,27 +1,40 @@
 ﻿using YoutubeExplode;
+using YoutubeExplode.Common;
 using YoutubeExplode.Converter;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
 
 namespace VideoGrabberLibrary;
 
 public class Download
 {
+    private YoutubeClient youtubeClient { get; set; }
+
+    public Download()
+    {
+        youtubeClient = new YoutubeClient();
+    }
 
     //string url = @"https://www.youtube.com/watch?v=W9vq4oOKYF8";
 
     public async Task GetVideo()
     {
-        string outputDirectory = @"C:\Users\kalnigi\Desktop";
+        string outputDirectory = @$"C:\Users\{Environment.UserName}\Desktop\";
         List<string> videoUrl = new()
         {
-            @"https://www.youtube.com/watch?v=W9vq4oOKYF8",
-            @"https://www.youtube.com/watch?v=W9vq4oOKYF8"
+            //@"https://www.youtube.com/watch?v=W9vq4oOKYF8",
+            //@"https://www.youtube.com/watch?v=NH8mmayZ1jA",
+            @"https://www.youtube.com/watch?v=9RIeycixkK8",
+            @"https://www.youtube.com/watch?v=Ghd2bkIadG4",
+            @"https://www.youtube.com/watch?v=KsthiOYDM9A",
+            //@"https://www.youtube.com/watch?v=JISQMhtXiSM&sttick=1"
         };
 
         try
         {
             foreach (string url in videoUrl)
             {
-                await YoutubeVideo(url, outputDirectory);
+                await GetMuxedVideo(url, outputDirectory, Container.WebM, ConversionPreset.UltraFast);
             }
         }
         catch (Exception ex) 
@@ -30,33 +43,140 @@ public class Download
         }
     }
 
-    private async Task YoutubeVideo(string url, string outputDirectory)
+    private async Task<IVideoStreamInfo> GetYoutubeVideo(string url, bool download = false)
     {
-        YoutubeClient youtube = new();
-        var video = await youtube.Videos.GetAsync(url);
+        IVideoStreamInfo streamVideoInfo = null;
 
-        string sanitizedTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
-
-        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
-        var muxedStreams = streamManifest.GetMuxedStreams().OrderByDescending(s => s.VideoQuality).ToList();
-
-        if (muxedStreams.Any())
+        try
         {
-            var streamInfo = muxedStreams.First();
-            using var httpClient = new HttpClient();
-            var stream = await httpClient.GetStreamAsync(streamInfo.Url);
-            var dateTime = DateTime.Now;
+            Dictionary<string, string> videoInfo = await GetVideoInformation(url);
+            StreamManifest streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(url);
 
-            string outputFilePath = Path.Combine(outputDirectory, $"{sanitizedTitle}.{streamInfo.Container}");
-            using var outputStream = File.Create(outputFilePath);
-            await stream.CopyToAsync(outputStream);
+            streamVideoInfo = streamManifest
+                .GetVideoOnlyStreams()
+                .Where(v => v.Container == Container.Mp4)
+                // .First(s => s.VideoQuality.Label == "1080p60")
+                .GetWithHighestVideoQuality();
 
-            Console.WriteLine("Download completed!");
-            Console.WriteLine($"Video saved as: {outputFilePath} - {dateTime}");
+            // Get stream
+            foreach (var item in streamManifest.Streams)
+            {
+                // info about available records
+                //Console.WriteLine(item);
+            }
+
+            //var stream = await youtube.Videos.Streams.GetAsync(streamVideoInfo);
+            //Console.WriteLine(video.Description);
+            //Console.WriteLine(await youtube.Videos.DownloadAsync(streamAudioInfo));
+
+            // Download stream to a file
+            //await youtube.Videos.Streams.DownloadAsync(streamAudioInfo, $"{outputDirectory}New.{streamAudioInfo.Container.Name}");
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine($"Not suitable video stream found for {video.Title}");
+            Console.WriteLine(ex);
         }
+
+        return streamVideoInfo;
+    }
+
+    private async Task<IStreamInfo> GetYoutubeAudio(string url, string outputDirectory, Container container, bool downloadAudio = false)
+    {
+        IStreamInfo streamAudioInfo = null;
+
+        try
+        {
+            Dictionary<string, string> videoInfo = await GetVideoInformation(url);
+            StreamManifest streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync(url);
+
+            if (downloadAudio == false)
+            {
+                streamAudioInfo = streamManifest
+                    .GetAudioStreams()
+                    .Where(a => a.Container == container)
+                    .GetWithHighestBitrate();
+            }
+            else
+            {
+                await youtubeClient.Videos.Streams.DownloadAsync(streamAudioInfo, $"{outputDirectory}{videoInfo["title"]}.{container.Name}");
+            }
+
+            // streamAudioInfo.Container.Name
+
+            // TODO: padaryti checkinika kokie conteineriai yra ir tik tada priimti pasirinkima
+            // Arba kai idedi url tada su event atnaujinti ir parodyti visus available containers
+            foreach (var item in streamManifest.Streams)
+            {
+                //Console.WriteLine(item.Container);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+        return streamAudioInfo;
+    }
+
+    private async Task<string> GetMuxedVideo(string url, string outputDirectory, Container containerType, ConversionPreset presetType)
+    {
+        string result = String.Empty;
+
+        try
+        {
+            IStreamInfo audioRecord =  await GetYoutubeAudio(url, outputDirectory, Container.Mp4);
+            IVideoStreamInfo videoRecord = await GetYoutubeVideo(url);
+            IStreamInfo[] streamInformation = { audioRecord, videoRecord };
+
+            Dictionary<string, string> videoInfo = await GetVideoInformation(url);
+
+            string finalOutputPath = $"{outputDirectory}{videoInfo["title"]}.{containerType.Name}";
+            string ffmpegPath = @$"C:\Users\{Environment.UserName}\Desktop\ffmpeg";
+
+            await youtubeClient.Videos
+                .DownloadAsync(streamInformation, new ConversionRequestBuilder(finalOutputPath)
+                .SetContainer(containerType)
+                .SetPreset(presetType)
+                .SetFFmpegPath(ffmpegPath)
+                .Build());
+
+            result = "It worked!";
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex);
+        }
+
+        return result;
+    }
+
+    private async Task<Dictionary<string, string>> GetVideoInformation (string url)
+    {
+        Dictionary<string, string> videoInformation = new();
+
+        try
+        {
+            Video video = await youtubeClient.Videos.GetAsync(url);
+            
+            string title = video.Title;
+            string author = video.Author.ToString();
+            string duration = video.Duration.ToString();
+            string description = video.Description;
+
+            // TODO: think how to clean the name without symbols
+
+            videoInformation.Add("title", title);
+            videoInformation.Add("author", author);
+            videoInformation.Add("duration", duration);
+            videoInformation.Add("description", description);
+        }
+        catch (Exception ex)
+        {
+            // loginima padaryti
+            Console.Error.WriteLine(ex);
+        }
+
+        return videoInformation;
     }
 }
